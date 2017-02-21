4a2ca91a4d35765985ec88b9313f160f1a1001d3
abab <ababup1192@gmail.com>
2017-02-21 13:07:31 +0900

    modify

--- gradle-test.sh
+++ gradle-test.sh
@@ -6,7 +6,10 @@ cp -r gradle-project git-repo/project
 cd git-repo
 docker-compose run gradle sh -c "cd /tmp/project && gradle test"
 cp -r ./project/build ../out
-ls ../out
+echo "aaaaa"
+ls ./project/build
+ls ../out/build
+echo "aaaaa"
 # status=$(echo $?)
 # exit $status
 
